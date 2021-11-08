9afade03df2b11b0f80572e45fa5d2a5983510ba
ixioph <ixioph@gmail.com>
2021-11-07 21:59:13 -0800

    mint implementation

--- contracts/Ship.sol
+++ contracts/Ship.sol
@@ -1,8 +1,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@chainlink/contracts/src/v0.8/VRFConsumerBase.sol';
 
-contract Ship is ERC1155 {
+contract Ship is ERC1155, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+
     struct Build {
         bytes22 name;
         uint8 body;
@@ -16,12 +22,42 @@ contract Ship is ERC1155 {
         uint8 c2;
         uint8 c3;
     }
-    Build s_ship;
+    //Build s_ship;
     Component[] public s_shipParts;
     CrewMember[] public s_shipCrew;
 
-    constructor("Space Ship", "SHIP") {
+    // mappings
+    mapping(bytes32 => address) public requestIdToSender;
+    mapping(bytes32 => string) public requestIdToTokenURI;
+    mapping(uint256 => Build) public tokenIdToShip;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+
+    constructor(address _VRFCoordinator, address _link, bytes32 _keyhash) public 
+    VRFConsumerBase(_VRFCoordinator, _link) 
+    ERC1155("Space Ship", "SHIP") {
+        tokenCounter = 0;
+        keyHash = _keyhash;
+        fee = 0.1 * 10**18;
+    }
+
+    function createCollectible(string memory tokenURI, uint256 seed) public 
+    returns (bytes32){
+        bytes32 requestId = requestRandomness(keyHash, fee, seed);
+        requestIdToSender[requestId] = msg.sender;
+        requestIdToTokenURI[requestId] = tokenURI;
         // ...
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 rNum) internal override {
+        address shipOwner = requestIdToSender[requestId];
+        string memory tokenURI = requestIdToTokenURI[requestId];
+        uint256 itemId = tokenCounter;
+        _safeMint(shipOwner, itemId);
+        _setTokenURI(itemId, tokenURI);
+        // break rNum up and use to populate Build variables
+        Build ship = Build();
+        tokenIdToShip[itemId] = ship;
+        requestIdToTokenId[requestId] = itemId;
     }
 
     remove_part();
